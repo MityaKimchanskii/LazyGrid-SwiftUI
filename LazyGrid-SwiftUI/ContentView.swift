1f20d072301a098bd0c107a29059a38a0f0c9701
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 18:59:48 -0700

    LazyVGrid with sections and NavigationView

--- LazyGrid-SwiftUI/ContentView.swift
+++ LazyGrid-SwiftUI/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+    
+    let columns: [GridItem] = [GridItem(.fixed(100)), GridItem(.fixed(100)), GridItem(.fixed(100))]
+    
+    private func headerView(_ index: Int) -> some View {
+        Text("Section \(index)")
             .padding()
+            .foregroundColor(.white)
+            .font(.title)
+            .frame(maxWidth: .infinity)
+            .background(.blue)
+    }
+    
+    var body: some View {
+        
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 10, pinnedViews: [.sectionHeaders]) {
+                    
+                    ForEach(1..<10) { index in
+                        
+                        Section(header: headerView(index)) {
+                            ForEach(1..<20) { _ in
+                                Rectangle()
+                                    .fill(.green)
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                        }
+                    }
+                }.padding()
+            }.navigationTitle("Navigation")
+        }
     }
 }
 
