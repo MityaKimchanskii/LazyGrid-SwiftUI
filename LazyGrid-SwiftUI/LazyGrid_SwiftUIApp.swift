3c39f0c97bae92804871f48cc9ced2d3c7c44b07
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 18:32:18 -0700

    Initial Commit

---
+++ LazyGrid-SwiftUI/LazyGrid_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGrid_SwiftUIApp.swift
+//  LazyGrid-SwiftUI
+//
+//  Created by Mitya Kim on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct LazyGrid_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
